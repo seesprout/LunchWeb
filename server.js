a410e790235e579e55b15eb32c8d6a742b2e941d
seesprout <122982928+seesprout@users.noreply.github.com>
2023-01-24 23:21:19 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -3,25 +3,14 @@ const cors = require('cors')
 const fs = require('fs')
 const http = require('http')
 const app = express()
-const port_Start = 5080 //StartPage 서버 포트 번호
-const port_map = 8080 //Map 서버 포트 번호
+const port = 8080 //서버 포트 번호
 const server = http.createServer(app)
 
 app.use(cors())
 
-app.get('http://localhost:8080', (req, res)=>{
-    fs.readFile('./map.html', function(err, data) {
-        if(err) {
-          res.send('Error')
-        } else {
-          res.writeHead(200, {'Content-Type':'text/html'})
-          res.write(data)
-          res.end()
-        }
-      })
-});
+app.use(express.static('public'));
 
-app.get('http://localhost:5080', (req, res)=>{
+app.get('/', (req, res)=>{
     fs.readFile('./Start.html', function(err, data) {
         if(err) {
           res.send('Error')
@@ -33,10 +22,18 @@ app.get('http://localhost:5080', (req, res)=>{
       })
 });
 
-server.listen(port_map, ()=>{
-    console.log(`서버가 실행됩니다. http://localhost:${port_map}`);
+app.get('/map', (req, res)=>{
+    fs.readFile('./map.html', function(err, data) {
+        if(err) {
+          res.send('Error')
+        } else {
+          res.writeHead(200, {'Content-Type':'text/html'})
+          res.write(data)
+          res.end()
+        }
+      })
 });
 
-server.listen(port_Start, ()=>{
-    console.log(`서버가 실행됩니다. http://localhost:${port_Start}`);
+server.listen(port, ()=>{
+    console.log(`서버가 실행됩니다. http://localhost:${ port }`);
 });
